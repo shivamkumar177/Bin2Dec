46d2f20b7b6a584843475c5aa81800e0bbd07a18
Shivam Kumar <shivamk1717@gmail.com>
2021-05-02 23:06:24 +0530

    added html and js

---
+++ main.js
@@ -0,0 +1,11 @@
+function Convert(){
+    var bin=(document.getElementById("binarybox").value);
+    var dec=0;
+    for(var i=bin.length-1;i>=0;i--){
+        if(bin[i]=='1'){
+            dec+=Math.pow(2,bin.length-i-1);
+        }
+    }
+    console.log(dec);
+    document.getElementById("decimalbox").value=dec;
+}
