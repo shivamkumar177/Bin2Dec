14ea289e098f032a258b5c8f1c49b15b149eacfc
Shivam Kumar <shivamk1717@gmail.com>
2022-03-31 00:12:55 +0530

    done

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function Convert(){
     var dec=0;
     for(var i=bin.length-1;i>=0;i--){
         if(bin[i]=='1'){
-            dec+=Math.pow(2,bin.length-i-1); 
+            dec+=Math.pow(2,bin.length-i-1);
         }
     }
     console.log(dec);
